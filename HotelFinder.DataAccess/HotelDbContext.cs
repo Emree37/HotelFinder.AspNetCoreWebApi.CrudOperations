81158f8c7a05c605248613a7360cb1e1dc44d084
bilisim.akademisi17 <bilisim.akademisi17@BILISIM17>
2021-09-01 15:52:16 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=BILISIM17\\SQLEXPRESS; Database=HotelDb; Trusted_Connection=True;");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
